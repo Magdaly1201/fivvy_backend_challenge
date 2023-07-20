51a3639a4467afd1a11f64212dc69d6d7b0c347c
Magdaly.santos <magdaly.santos@tenpo.cl>
2023-07-20 20:36:20 -0300

    first commit base api

---
+++ solution/fivvy/src/main/java/com/magdy/challlenge/fivvy/FivvyApplication.java
@@ -0,0 +1,13 @@
+package com.magdy.challlenge.fivvy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FivvyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FivvyApplication.class, args);
+	}
+
+}
---
+++ solution/fivvy/src/test/java/com/magdy/challlenge/fivvy/FivvyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magdy.challlenge.fivvy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FivvyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
